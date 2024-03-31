2840a765559502485e44da0337e3adc03afb40fe
Ashley B <109474630+AshB4@users.noreply.github.com>
2024-03-30 21:48:52 -0500

    List Of Customers Fully operational

--- src/ApiCalls/customerCrud.js
+++ src/ApiCalls/customerCrud.js
@@ -1,50 +1,56 @@
 import axios from "axios";
+import { getCookie } from "../Utils/cookie";
 
-const API_BASE_URL =
-	process.env.REACT_APP_API_URL ||
-	"http://127.0.0.1:8000/customer-list/"; 
+const API_BASE_URL = "http://localhost:8000/customer-list/";
 
-async function fetchCustomers() {
-  try {
-    const response = await axios.get(API_BASE_URL);
-    return response.data;
-  } catch (error) {
-    console.error("Error fetching customers:", error);
-    throw new Error("An error occurred while fetching customers.");
-  }
+async function fetchCustomer() {
+  const csrftoken = getCookie("csrftoken");
+	const headers = { "X-CSRFToken": csrftoken };
+	try {
+    console.log("Fetching List of Customers")
+		const response = await axios.get(API_BASE_URL, { headers });
+    console.log("List of Customer fetched successfully:", response.data);
+		return response.data;
+	} catch (error) {
+		console.error("Error fetching customer list:", error);
+		throw new Error("An error occurred while fetching customer list.");
+	}
 }
 
 async function createCustomer(customerData) {
-  try {
-    const response = await axios.post(API_BASE_URL, customerData);
-    return response.data;
-  } catch (error) {
-    console.error("Error creating customer:", error);
-    throw new Error("An error occurred while creating the customer.");
-  }
+	try {
+    console.log("Creating Customer List:" , customerData)
+		const response = await axios.post(API_BASE_URL, customerData);
+    console.log("Customer list created successfully", response.data)
+		return response.data;
+	} catch (error) {
+		console.error("Error creating customer:", error);
+		throw new Error("An error occurred while creating the customer.");
+	}
 }
 
-async function updateCustomer( customerId, updatedData) {
-  try {
-    const response = await axios.put(
-      `${API_BASE_URL}/${customerId}`,
-      updatedData
-    );
-    return response.data;
-  } catch (error) {
-    console.error("Error updating customer:", error);
-    throw new Error("An error occurred while updating the customer.");
-  }
+async function updateCustomer(customer_id, updatedData) {
+	try {
+    console.log("Updating Customer List:", customer_id)
+		const response = await axios.put(`${API_BASE_URL}${customer_id}`,updatedData);
+    console.log("Customer list updated sucessfully:", response.data)
+		return response.data;
+	} catch (error) {
+		console.error("Error updating customer:", error);
+		throw new Error("An error occurred while updating the customer.");
+	}
 }
 
 async function deleteCustomer(customerId) {
-  try {
-    await axios.delete(`${API_BASE_URL}/${customerId}`);
-    return true; // Or you can return some indication of success
-  } catch (error) {
-    console.error("Error deleting customer:", error);
-    throw new Error("An error occurred while deleting the customer.");
-  }
+	try {
+    console.log("Deleteing Customer from List");
+    const response = await axios.delete(`${API_BASE_URL}${customerId}`);
+    console.log("Customer list deleted sucessfully:", response.data)
+    return response.data;
+	} catch (error) {
+		console.error("Error deleting customer from list:", error);
+		throw new Error("An error occurred while deleting the customer from the list.");
+	}
 }
 
-export { fetchCustomers, createCustomer, updateCustomer, deleteCustomer };
+export { fetchCustomer, createCustomer, updateCustomer, deleteCustomer };
--- src/Components/customer-list.js
+++ src/Components/customer-list.js
@@ -1,19 +1,23 @@
 /** @format */
 
 import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import axios from "axios";
+import { getCookie } from "../Utils/cookie";
 import {
-	fetchCustomers,
+	fetchCustomer,
 	createCustomer,
 	updateCustomer,
 	deleteCustomer,
 } from "../ApiCalls/customerCrud";
 
 function CustomerList() {
+	const { id } = useParams();
 	const [customers, setCustomers] = useState([]);
 	const [isLoading, setIsLoading] = useState(false);
 	const [error, setError] = useState(null);
 	const [newCustomerData, setNewCustomerData] = useState({
-		customer_id: '',
+		customer_id: "",
 		name: "",
 		contact: "",
 		email: "",
@@ -25,12 +29,11 @@ function CustomerList() {
 		setError(null);
 
 		try {
-			const data = await fetchCustomers();
+			const data = await fetchCustomer(id);
 			setCustomers(data);
-			console.log(data)
 		} catch (error) {
-			console.error("Error fetching customers:", error);
-			setError("An error occurred while fetching customers.");
+			console.error("Error fetching list of customers:", error);
+			setError("An error occurred while fetching list of customers.");
 		} finally {
 			setIsLoading(false);
 		}
@@ -38,7 +41,7 @@ function CustomerList() {
 
 	useEffect(() => {
 		fetchData();
-	}, []);
+	}, [id]);
 
 	const handleInputChange = (e) => {
 		const { name, value } = e.target;
@@ -48,10 +51,9 @@ function CustomerList() {
 		});
 	};
 
-	const handleUpdateCustomer = async () => {
+	const handleAddCustomer = async () => {
 		try {
 			await createCustomer(newCustomerData);
-			fetchData();
 			setNewCustomerData({
 				customer_id: "",
 				name: "",
@@ -59,124 +61,142 @@ function CustomerList() {
 				email: "",
 				phone: "",
 			});
-		} catch (error) {
-			console.error("Error creating customer:", error);
-			setError("An error occurred while creating the customer.");
-		}
-	};
-
-	const handleDeleteCustomer = async (customer) => {
-		try {
-			await deleteCustomer(customer);
 			fetchData();
 		} catch (error) {
-			console.error("Error deleting customer:", error);
-			setError("An error occurred while deleting the customer.");
+			console.error("Error creating customer information:", error);
+			setError("An error occurred while creating customer information.");
 		}
 	};
 
-	return (
-		<div className="topDiv">
-			<div className="container">
-				<div className="titles">
-					<h2 className="form-titles">CUSTOMER LIST</h2>
-				</div>
-				<br />
-				<div className="sections">
-					<div>
-						<br />
-						<h3>EDIT INFORMATION:</h3>
-						{isLoading && <p>Loading customers...</p>}
-						{error && (
-							<div className="error-container">
-								<p className="error">{error}</p>
-							</div>
-						)}
-						<div className="inputs-form">
-							<div className="inputs">
-								<input
-									type="text"
-									name="customer_id"
-									value={newCustomerData.customer_id}
-									onChange={handleInputChange}
-									placeholder="Customer ID"
-								/>
-							</div>
-						<div className="inputs-form">
-							<div className="inputs">
-								<input
-									type="text"
-									name="name"
-									value={newCustomerData.name}
-									onChange={handleInputChange}
-									placeholder="Name"
-								/>
-							</div>
-							<div className="inputs">
-								<input
-									type="text"
-									name="contact"
-									value={newCustomerData.contact}
-									onChange={handleInputChange}
-									placeholder="Contact"
-								/>
-							</div>
-							<div className="inputs">
-								<input
-									type="email"
-									name="email"
-									value={newCustomerData.email}
-									onChange={handleInputChange}
-									placeholder="Email"
-								/>
-							</div>
-							<div className="inputs">
-								<input
-									type="tel"
-									name="phone"
-									value={newCustomerData.phone}
-									onChange={handleInputChange}
-									placeholder="Phone"
-								/>
-							</div>
-							<div className="yellow-button">
-								<button className="buttons" onClick={handleUpdateCustomer}>
-									ADD CUSTOMER
-								</button>
+const handleUpdateCustomer = async (customerId , updatedData) => {
+		try{
+					await updateCustomer(customerId, updatedData);
+					fetchData();
+		} catch (error){
+			console.error("Error updating list of customers")
+			setError("An error occurred while updating the list of customers.");
+		}
+};	
 
-								<button className="buttons" onClick={handleDeleteCustomer}>
-									DELETE
-								</button>
-							</div>
+	const handleDeleteCustomer = async (customerId) => {
+		try {
+			const csrftoken = getCookie("csrftoken");
+			const headers = { "X-CSRFToken": csrftoken };
+			await axios.delete(`http://localhost:8000/customer-list/${customerId}`, {
+				headers,
+			});
+			console.log("Customer deleted successfully");
+			fetchData();
+		} catch (error) {
+			console.error("Error deleting the customer list :", error);
+			setError("An error occurred while deleting the customerlist .");
+		}
+	};
+
+return (
+		<div className="container">
+			<h2 className="form-titles">LIST OF CUSTOMERS</h2>
+			<ul style={{ display: "none" }}>
+				{customers.map((customer) => (
+					<li key={customer.customer_id}>
+						<Link to={`http://localhost:8000/customer-list/${customer.customer_id}`} />
+					</li>
+				))}
+			</ul>
+			<div className="sections">
+				<br />
+				<div>
+					<br />
+					<h3>EDIT CUSTOMER INFORMATION:</h3>
+					{isLoading && <p>Loading customers...</p>}
+					{error && <p>Error: {error}</p>}
+					<br />
+					<div className="inputs-form">
+						<div className="inputs">
+							<input
+								type="number"
+								name="customer_id"
+								value={newCustomerData.customer_id}
+								onChange={handleInputChange}
+								placeholder="Customer ID"
+							/>
 						</div>
-						<br />
-						<table>
-							<thead>
-								<tr>
-									<th>Customer ID</th>
-									<th>Name</th>
-									<th>Contact</th>
-									<th>Email</th>
-									<th>Phone</th>
-								</tr>
-							</thead>
-							<tbody>
-								{customers.map((customer) => (
-									<tr key={customer.customer_id}>
-										<td>{customer.customer_id}</td>
-										<td>{customer.name}</td>
-										<td>{customer.contact}</td>
-										<td>{customer.email}</td>
-										<td>{customer.phone}</td>
-										<td></td>
-									</tr>
-								))}
-							</tbody>
-						</table>
+						<div className="inputs">
+							<input
+								type="text"
+								name="name"
+								value={newCustomerData.name}
+								onChange={handleInputChange}
+								placeholder="Name"
+							/>
+						</div>
+						<div className="inputs">
+							<input
+								type="text"
+								name="contact"
+								value={newCustomerData.contact}
+								onChange={handleInputChange}
+								placeholder="Contact"
+							/>
+						</div>
+						<div className="inputs">
+							<input
+								type="email"
+								name="email"
+								value={newCustomerData.email}
+								onChange={handleInputChange}
+								placeholder="Email"
+							/>
+						</div>
+						<div className="inputs">
+							<input
+								type="tel"
+								name="phone"
+								value={newCustomerData.phone}
+								onChange={handleInputChange}
+								placeholder="Phone"
+							/>
+						</div>
+						<div className="yellow-button">
+							<button className="buttons" onClick={handleAddCustomer}>
+								ADD
+							</button>
+						</div>
 					</div>
+					<br />
+					<table>
+						<thead>
+							<tr>
+								<th>Customer ID</th>
+								<th>Name</th>
+								<th>Contact</th>
+								<th>Email</th>
+								<th>Phone</th>
+								<th>Actions</th>
+							</tr>
+						</thead>
+						<tbody>
+							{customers.map((customer) => (
+								<tr key={customer.customer_id}>
+									<td>{customer.customer_id}</td>
+									<td>{customer.name}</td>
+									<td>{customer.contact}</td>
+									<td>{customer.email}</td>
+									<td>{customer.phone}</td>
+									<td>
+										<button
+											className="buttons"
+											onClick={() => handleDeleteCustomer(customer.customer_id)}
+										>
+											DELETE
+										</button>
+									</td>
+								</tr>
+							))}
+						</tbody>
+					</table>
 				</div>
 			</div>
-		</div>
 		</div>
 	);
 }
